0fb0f944073daf7c2baba1504a22d2746676a5e4
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-06-01 13:09:46 -0300

    finalization the metrics

--- src/api/get-daily-revenue-in-period.ts
+++ src/api/get-daily-revenue-in-period.ts
@@ -1,11 +1,24 @@
 import { api } from '@/lib/axios'
+export interface GetDailyRevenueInPeriodQuery {
+  from?: Date
+  to?: Date
+}
 export type GetDailyRevenueInPeriod = {
   date: string
   receipt: number
 }[]
-export async function getDailyRevenueInPeriod() {
+export async function getDailyRevenueInPeriod({
+  from,
+  to,
+}: GetDailyRevenueInPeriodQuery) {
   const response = await api.get<GetDailyRevenueInPeriod>(
     'metrics/daily-receipt-in-period',
+    {
+      params: {
+        from,
+        to,
+      },
+    },
   )
   return response.data
 }
